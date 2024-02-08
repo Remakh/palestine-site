c29ca2eeb5c257ccdd445a0bc8166058dc568b28
remakh <reean.m@gmail.com>
2024-02-08 22:55:55 +0000

    fix this later

--- src/components/EventHeader.jsx
+++ src/components/EventHeader.jsx
@@ -7,7 +7,7 @@ import { s1 } from '../assets'
 import { VscCalendar } from "react-icons/vsc";
 import { FaClock } from "react-icons/fa";
 import { FaSearchLocation } from "react-icons/fa";
-import google_key from "../key.js"
+// import google_key from "../key.js"
 
 
 export async function loader( {params} ) {
@@ -41,7 +41,7 @@ const EventHeader = () => {
                         loading="lazy"
                         allowfullscreen
                         referrerpolicy="no-referrer-when-downgrade"
-                        src={`https://www.google.com/maps/embed/v1/place?key=${google_key}
+                        src={`https://www.google.com/maps/embed/v1/place?key=$
                             &q=${event.location}`}>
                     </iframe>
                 </div>
